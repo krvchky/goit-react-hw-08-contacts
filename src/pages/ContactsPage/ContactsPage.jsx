23cf1041fc7dc9b1cb8c443c43858cd1e47a1d82
krvchky <kravchukyana05@gmail.com>
2024-08-30 13:16:25 +0200

    Update ContactsPage.jsx

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,16 +1,51 @@
+// import { useEffect } from "react";
+// import ContactForm from "../../components/ContactForm/ContactForm";
+// import ContactList from "../../components/ContactList/ContactList";
+// import SearchBox from "../../components/SearchBox/SearchBox";
+// import { fetchContacts } from "../../redux/contacts/operations";
+// import s from "./ContactsPage.module.css";
+// import { useDispatch } from "react-redux";
+
+// const ContactsPage = () => {
+//   const dispatch = useDispatch();
+//   useEffect(() => {
+//     dispatch(fetchContacts());
+//   }, [dispatch]);
+
+//   return (
+//     <>
+//       <div className={s.bg}>
+//         <ContactForm />
+//         <SearchBox />
+//         <ContactList />
+//       </div>
+//     </>
+//   );
+// };
+
+// export default ContactsPage;
 import { useEffect } from "react";
 import ContactForm from "../../components/ContactForm/ContactForm";
 import ContactList from "../../components/ContactList/ContactList";
 import SearchBox from "../../components/SearchBox/SearchBox";
 import { fetchContacts } from "../../redux/contacts/operations";
 import s from "./ContactsPage.module.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
 
 const ContactsPage = () => {
   const dispatch = useDispatch();
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
   useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
+    if (isLoggedIn) { 
+      dispatch(fetchContacts());
+    }
+  }, [dispatch, isLoggedIn]);
+
+  if (!isLoggedIn) {
+    return <p>Please log in to view your contacts.</p>; 
+  }
 
   return (
     <>
